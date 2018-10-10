5291a4f145acaac52605f3818d78223dc8ff2169
akc52 <a_lys_sa@hotmail.com>
2018-10-10 16:30:09 -0400

    Implement fetchJsonp

--- src/SelectedProduct/AvailableStoresList/index.js
+++ src/SelectedProduct/AvailableStoresList/index.js
@@ -6,20 +6,15 @@ const AvailableStoresList = props => (
   <div className="storeList__Container">
     <h3>Available at these LCBO's:</h3>
     <ul className="storeList">
-    {/*
-      TO DO this filtering should not be here, in app.js on fetch
-
-      ALSO id/description/image should be set in App.js from products array
-    */}
       {props.availableStores
-      .map((store) =>
-        <AvailableStore
-          key={store.id}
-          address1={store.address_line_1}
-          address2={store.address_line_2}
-          city={store.city}
-          storeName={store.name}
-          postalCode={store.postal_code} />
+        .map((store) =>
+          <AvailableStore
+            key={store.id}
+            address1={store.address_line_1}
+            address2={store.address_line_2}
+            city={store.city}
+            storeName={store.name}
+            postalCode={store.postal_code} />
       )}
     </ul>
   </div>
